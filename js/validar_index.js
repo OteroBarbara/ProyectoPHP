0d6a5642a763ea6f6d6e669d43afecec755a2ea1
Bárbara <oterobarbara251@gmail.com>
2020-10-19 14:34:28 -0300

    js para index

--- js/validar_index.js
+++ js/validar_index.js
@@ -1,16 +1,13 @@
-function validar_index () {
-    var usuario = document.getElementById("user");
-    var contraseña = document.getElementById("pword");
-    if (usuario.length < 6) {
-        alert("El usuario tiene menos de 6 caracteres");
+function validarindex () {
+    var usuario = document.getElementById("user").value;
+    var pass = document.getElementById("pword").value;
+
+    if (!((/^\w{6,50}$/gm).test(usuario))) {
+        alert("El usuario debe tener entre 6 y 50 caracteres alfanuméricos");
         return false;
     }
-    else if (usuario.length < 6) {
-        alert("El usuario tiene menos de 6 caracteres");
-        return false;
-    }
-    else if (contraseña.length < 6) {
-        alert("La contraseña tiene menos de 6 caracteres");
+    else if (!((/^\w{6,50}$/gm).test(pass))) {
+        alert("La contraseña debe tener entre 6 y 50 caracteres");
         return false;
     }
     else {
